ff382139070dd5fbd87e06d69dfcf7c2c5c84f41
Justin Ko <justko62@gmail.com>
2015-07-06 23:30:52 -0700

    Added range to row selection

--- factor_analysis.py
+++ factor_analysis.py
@@ -75,27 +75,20 @@ def varimax_rotation(matrix, eps=1e-6, itermax=1000):
     return output_matrix
 
 
-def pca(dataframe, var_x='', var_y='', stop=-1, rotation='varimax', return_factors=False):
+def pca(dataframe, var_x, var_y, stop=-1, rotation='varimax', return_factors=False):
     """
     Principal Component Analysis implementation using the correlation matrix. This could be expanded to use the
     covariance matrix by adding an if statement when the correlation matrix is created.
     :param dataframe: Entire datafile. Session ID should be the index
-    :param var_x: First variable of the range to be included in the analysis. Defaults to the first variable
-    :param var_y: Last variable of the range to be included in the analysis. Defaults to the last variable
+    :param var_x: First variable of the range to be included in the analysis.
+    :param var_y: Last variable of the range to be included in the analysis.
     :param stop: Criteria to stop. Integer number indicating the number of factors to be included.
     :param rotation: which rotation to perform. If False, none will be done. Default is Varimax
     :param return_factors: If True, returns factors and prints out loadings
     :return: Table of factor loadings, whether rotated or unrotated.
     """
     # Load the initial data frame
-    initial_df = dataframe.copy()
-    if var_x != '' and var_y != '':
-        initial_df = initial_df.loc[:, var_x : var_y]
-    elif var_x != '':
-        initial_df = initial_df.loc[:, var_x : ]
-    elif var_y != '':
-        initial_df = initial_df.loc[:, : var_y]
-
+    initial_df = dataframe.loc[:, var_x : var_y]
 
     # Computing the correlation matrix, finding the eigenvalues and eigenvectors, then sorting them
     corr_matrix = np.corrcoef(initial_df, rowvar=0)
@@ -164,7 +157,7 @@ def pca(dataframe, var_x='', var_y='', stop=-1, rotation='varimax', return_facto
         for r in dataframe.iterrows():
             factors = {}
             for c in range(0, len(loadings.columns)):
-                factors['Factor ' + str(c+1)] = loadings.ix[:, c].multiply(r[1]).sum()
+                factors['Factor ' + str(c+1)] = loadings.ix[:, c].multiply(r[1][var_x:var_y]).sum()
             factors_series = pd.Series(data=factors, name=dataframe.index[index_name_i])
             factor_sums = factor_sums.append(factors_series)
             index_name_i += 1
